9e99ff3a9cd8b99f69e23cdf0d5dd76211189c51
Omar Benka <benka.dev@gmail.com>
2020-08-25 14:45:17 +0200

    Ajout Edit/Udate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
-
+    
     def show
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        current_user.update(user_params)
+        redirect_to current_user
+    end
+
+    private
+    
+    def user_params
+    params.require(:user).permit(:username, :website,
+                                :bio, :email, :phone, :gender)
     end
     
 end
