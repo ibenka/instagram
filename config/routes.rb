0affee61224267f8eb5b0db44e7620260e3ad5b9
Omar Benka <benka.dev@gmail.com>
2020-08-25 16:12:54 +0200

    Création nouveau post et validation

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
